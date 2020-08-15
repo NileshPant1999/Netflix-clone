bf18b922048230a5002f1868f225813b96ed669e
Nilesh Pant <nileshpant6969@gmail.com>
2020-08-15 13:11:27 +0530

    added some styling

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        This is a netflix clone
-        <Row
-          title="NETFLIX ORIGINALS"
-          fetchUrl={requests.fetchNetflixOriginals}
-        />
-        <Row title="Trending Now" fetchUrl={requests.fetchTrendings} />
-      </h1>
+    <div className="app">
+      <h1>Netflix Clone</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrendings} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentries} />
     </div>
   );
 }
